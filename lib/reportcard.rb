b90553de0e24baaa3b9c8a79706f5d2e39962290
gentlemn <sonalkumar74@gmail.com>
2023-12-03 02:47:45 +0530

    first commit

---
+++ lib/reportcard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "reportcard/version"
+
+module Reportcard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/reportcard/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Reportcard
+  VERSION = "0.1.0"
+end
